c570cf66a070f19b6a26e9b5c2794a4dfb3c3098
niladhikari <niladhikari445@gmail.com>
2023-10-19 02:46:00 +0600

    First the get operation is executed, through which the id of each product is found. Later through this client side send this data according to single Eid. After that, if the updated data is sent from the client side, then the updated data is sent to the server through the PUt operation.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection URI
-const uri = "mongodb+srv://brandShop:hlztDDmDc5TH7EhI@cluster0.fcmyfrv.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://brandShop:hlztDDmDc5TH7EhI@cluster0.fcmyfrv.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -48,7 +49,9 @@ async function run() {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const brandNameFromData = await brandCollection.findOne(filter);
-      const result = await productCollection.find({ brandName: brandNameFromData.brandName }).toArray();
+      const result = await productCollection
+        .find({ brandName: brandNameFromData.brandName })
+        .toArray();
       res.send(result);
     });
 
@@ -66,9 +69,43 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const products = await productCollection.findOne(query);
+      console.log(products);
+      res.send(products);
+    });
+
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = {
+        $set: {
+          name: data.name,
+          brandName: data.brandName,
+          type: data.type,
+          price: data.price,
+          rating: data.rating,
+          details: data.details,
+          photo: data.photo,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updatedProduct,
+        options
+      );
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensure that the client will close when you finish/error
     // await client.close();
@@ -80,3 +117,94 @@ run().catch(console.error);
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
+
+// const express = require("express");
+// const cors = require("cors");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// require("dotenv").config();
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // MongoDB connection URI
+// const uri = "mongodb+srv://brandShop:hlztDDmDc5TH7EhI@cluster0.fcmyfrv.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//       version: ServerApiVersion.v1,
+//       strict: true,
+//       deprecationErrors: true,
+//     },
+//   });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+
+//     const brandCollection = client.db("brandDB").collection("brands");
+//     const productCollection = client.db("brandDB").collection("products");
+
+//     // POST request to add a brand
+//     app.post("/brand", async (req, res) => {
+//       const user = req.body;
+//       const result = await brandCollection.insertOne(user);
+//       res.send(result);
+//     });
+
+//     // GET request to retrieve a list of brands
+//     app.get("/brand", async (req, res) => {
+//       const cursor = brandCollection.find();
+//       const result = await cursor.toArray();
+//       res.send(result);
+//     });
+
+//     app.get("/brand/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const filter = { _id: new ObjectId(id) };
+//       const brandNameFromData = await brandCollection.findOne(filter);
+//       const result = await productCollection
+//         .find({ brandName: brandNameFromData.brandName })
+//         .toArray();
+//       res.send(result);
+//     });
+
+//     // POST request to add a product
+//     app.post("/product", async (req, res) => {
+//       const user = req.body;
+//       const result = await productCollection.insertOne(user);
+//       res.send(result);
+//     });
+
+//     // GET request to retrieve a list of products
+//     app.get("/product", async (req, res) => {
+//       const cursor = productCollection.find();
+//       const result = await cursor.toArray();
+//       res.send(result);
+//     });
+
+//     app.get("/product/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: new ObjectId(id) };
+//       const products = await productCollection.findOne(query);
+//       res.send(products);
+//     });
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensure that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+
+// run().catch(console.error);
+
+// app.listen(port, () => {
+//   console.log(`App is running on port ${port}`);
+// });
