6d13627f0bb83cc462ba1ef1befbe57ff46fc6f2
niladhikari <niladhikari445@gmail.com>
2023-10-20 01:18:06 +0600

    add the post nethod for the userCollection to collect the clint side sign in email information ....then create another collection to get the add card information for the user ways

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ async function run() {
 
     const brandCollection = client.db("brandDB").collection("brands");
     const productCollection = client.db("brandDB").collection("products");
+    const userCollection = client.db("brandDB").collection("users");
+    const myCardCollection = client.db("brandDB").collection("card");
+
+    //
+    app.post("/cards", async (req, res) => {
+      const product = req.body;
+      const result = await myCardCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.get("/cards/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const result = await myCardCollection.find({email: email}).toArray();
+      res.send(result);
+    });
+
+    
+    
 
     // POST request to add a brand
     app.post("/brand", async (req, res) => {
@@ -101,6 +120,15 @@ async function run() {
       res.send(result);
     });
 
+
+    // for the user collection
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -118,93 +146,3 @@ app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
 
-// const express = require("express");
-// const cors = require("cors");
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// require("dotenv").config();
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // MongoDB connection URI
-// const uri = "mongodb+srv://brandShop:hlztDDmDc5TH7EhI@cluster0.fcmyfrv.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       strict: true,
-//       deprecationErrors: true,
-//     },
-//   });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server (optional starting in v4.7)
-//     await client.connect();
-
-//     const brandCollection = client.db("brandDB").collection("brands");
-//     const productCollection = client.db("brandDB").collection("products");
-
-//     // POST request to add a brand
-//     app.post("/brand", async (req, res) => {
-//       const user = req.body;
-//       const result = await brandCollection.insertOne(user);
-//       res.send(result);
-//     });
-
-//     // GET request to retrieve a list of brands
-//     app.get("/brand", async (req, res) => {
-//       const cursor = brandCollection.find();
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/brand/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const filter = { _id: new ObjectId(id) };
-//       const brandNameFromData = await brandCollection.findOne(filter);
-//       const result = await productCollection
-//         .find({ brandName: brandNameFromData.brandName })
-//         .toArray();
-//       res.send(result);
-//     });
-
-//     // POST request to add a product
-//     app.post("/product", async (req, res) => {
-//       const user = req.body;
-//       const result = await productCollection.insertOne(user);
-//       res.send(result);
-//     });
-
-//     // GET request to retrieve a list of products
-//     app.get("/product", async (req, res) => {
-//       const cursor = productCollection.find();
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/product/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const products = await productCollection.findOne(query);
-//       res.send(products);
-//     });
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensure that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-
-// run().catch(console.error);
-
-// app.listen(port, () => {
-//   console.log(`App is running on port ${port}`);
-// });
